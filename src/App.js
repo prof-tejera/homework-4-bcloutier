33b2ef8e5f3fb79a4361eb9d6fbeba384418df80
Brandon Cloutier <brandon.cloutier@gmail.com>
2021-10-04 18:42:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Calculator from 'components/Calculator';
+import "./App.css";
+import Calculator from "components/Calculator";
 
 function App() {
   return (
     <div className="App">
       <header>Calculator</header>
       <Calculator />
+      <div>Hello World</div>
     </div>
   );
 }
